428dae0221d79ccecb8c949cf942bff5ba0064e0
Alina <92506045+AlinaSchumacher@users.noreply.github.com>
2025-05-09 18:50:09 +0200

    Animation Fixes + internal movement states

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,6 +9,9 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField]
     private float runSpeed;
+
+    [SerializeField]
+    private float sprintSpeed;
 
     [SerializeField]
     private float crouchWalkSpeed;
@@ -21,17 +25,25 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float jumpSpeed;
 
+    private enum MovementState
+    {
+        Default,
+        Sprinting,
+        Crouching,
+    }
+
     private Rigidbody rb;
     private Vector3 moveDir;
     private bool isOnGround;
-    private bool IsCrouching = false;
+    private MovementState state = MovementState.Default;
     private float targetJumpY;
     private bool jumped;
 
-    public event EventHandler<bool> OnIsWalking;
+    // public event EventHandler<bool> OnIsWalking;
     public event EventHandler<bool> OnIsRunning;
     public event EventHandler<bool> OnIsFalling;
     public event EventHandler<bool> OnIsCrouching;
+    public event EventHandler<bool> OnIsSprinting;
     public event EventHandler OnJumped;
 
     private void Awake()
@@ -39,6 +51,8 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         targetJumpY = 0;
     }
+
+    private void Update() { }
 
     private void FixedUpdate()
     {
@@ -80,20 +94,52 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector3 currentPos = rb.position;
         Vector3 newPos;
-        if (IsCrouching)
-            newPos = currentPos + moveDir * crouchWalkSpeed * Time.deltaTime;
-        else
-        {
-            newPos = currentPos + moveDir * runSpeed * Time.deltaTime;
+        float speedVal;
 
-            //jump
-            if (jumped)
-                newPos += Vector3.up * jumpSpeed * Time.deltaTime;
-            if (currentPos.y >= targetJumpY)
-                jumped = false;
+        switch (state)
+        {
+            case MovementState.Crouching:
+                speedVal = crouchWalkSpeed;
+                break;
+            case MovementState.Sprinting:
+                speedVal = sprintSpeed;
+                break;
+            default:
+                speedVal = runSpeed;
+                break;
         }
 
+        newPos = currentPos + moveDir * speedVal * Time.deltaTime;
+
+        //jump
+        if (jumped)
+            newPos += Vector3.up * jumpSpeed * Time.deltaTime;
+        if (currentPos.y >= targetJumpY)
+            jumped = false;
+
         rb.MovePosition(newPos);
+    }
+
+    private void SetStateCrouching(bool isCrouching, MovementState change = MovementState.Default)
+    {
+        state = (isCrouching) ? MovementState.Crouching : change;
+        OnIsCrouching?.Invoke(this, isCrouching);
+    }
+
+    public bool IsCrouching()
+    {
+        return state == MovementState.Crouching;
+    }
+
+    private void SetStateSprinting(bool isSprinting, MovementState change = MovementState.Default)
+    {
+        state = (isSprinting) ? MovementState.Sprinting : change;
+        OnIsSprinting?.Invoke(this, isSprinting);
+    }
+
+    public bool IsSprinting()
+    {
+        return state == MovementState.Sprinting;
     }
 
     private void OnMove(InputValue v)
@@ -114,7 +160,9 @@ public class PlayerMovement : MonoBehaviour
         if (val == new Vector2(0, 0))
         {
             OnIsRunning?.Invoke(this, false);
-            OnIsWalking?.Invoke(this, false);
+            // OnIsWalking?.Invoke(this, false);
+            if (IsSprinting())
+                SetStateSprinting(false);
         }
         else
         {
@@ -124,20 +172,38 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnJump()
     {
-        if (isOnGround && !IsCrouching)
+        if (isOnGround)
         {
             OnJumped?.Invoke(this, EventArgs.Empty);
             targetJumpY = rb.position.y + jumpHeight;
             jumped = true;
+
+            if (IsCrouching())
+                SetStateCrouching(false);
         }
     }
 
-    private void OnCrouch(InputValue v)
+    private void OnCrouch()
     {
         if (isOnGround)
         {
-            IsCrouching = v.Get<float>() == 1;
-            OnIsCrouching?.Invoke(this, IsCrouching);
+            SetStateCrouching(!IsCrouching());
+            SetStateSprinting(
+                false,
+                (IsCrouching()) ? MovementState.Crouching : MovementState.Default
+            );
+        }
+    }
+
+    private void OnSprint()
+    {
+        if (isOnGround)
+        {
+            SetStateSprinting(!IsSprinting());
+            SetStateCrouching(
+                false,
+                (IsSprinting()) ? MovementState.Sprinting : MovementState.Default
+            );
         }
     }
 }
--- Assets/Scripts/PlayerMovementAnimation.cs
+++ Assets/Scripts/PlayerMovementAnimation.cs
@@ -7,29 +7,34 @@ public class PlayerMovementAnimation : MonoBehaviour
     private const String IS_RUNNING = "IsRunning";
     private const String IS_FALLING = "IsFalling";
     private const String IS_CROUCHING = "IsCrouching";
+    private const String IS_SPRINTING = "IsSprinting";
     private const String IS_LANDING = "IsLanding";
     private const String JUMP = "Jump";
 
     [SerializeField]
     private Animator animator;
 
-    private PlayerMovement playerMovement;
+    private Transform player;
 
     private void Start()
     {
-        playerMovement = transform.GetComponentInParent<PlayerMovement>();
+        player = transform.parent;
 
-        playerMovement.OnIsWalking += PlayerMovement_OnIsWalking;
-        playerMovement.OnIsRunning += PlayerMovement_OnIsRunning;
-        playerMovement.OnIsFalling += PlayerMovement_OnIsFalling;
-        playerMovement.OnIsCrouching += PlayerMovement_OnIsCrouching;
-        playerMovement.OnJumped += PlayerMovement_OnJump;
+        if (player.TryGetComponent<PlayerMovement>(out PlayerMovement playerMovement))
+        {
+            // playerMovement.OnIsWalking += PlayerMovement_OnIsWalking;
+            playerMovement.OnIsRunning += PlayerMovement_OnIsRunning;
+            playerMovement.OnIsFalling += PlayerMovement_OnIsFalling;
+            playerMovement.OnIsCrouching += PlayerMovement_OnIsCrouching;
+            playerMovement.OnIsSprinting += PlayerMovement_OnIsSprinting;
+            playerMovement.OnJumped += PlayerMovement_OnJump;
+        }
     }
 
-    private void PlayerMovement_OnIsWalking(object sender, bool isWalking)
-    {
-        // animator.SetBool(IS_WALKING, isWalking);
-    }
+    // private void PlayerMovement_OnIsWalking(object sender, bool isWalking)
+    // {
+    //     animator.SetBool(IS_WALKING, isWalking);
+    // }
 
     private void PlayerMovement_OnIsRunning(object sender, bool isRunning)
     {
@@ -51,5 +56,10 @@ public class PlayerMovementAnimation : MonoBehaviour
     private void PlayerMovement_OnIsCrouching(object sender, bool isCrouching)
     {
         animator.SetBool(IS_CROUCHING, isCrouching);
+    }
+
+    private void PlayerMovement_OnIsSprinting(object sender, bool isSprinting)
+    {
+        animator.SetBool(IS_SPRINTING, isSprinting);
     }
 }
